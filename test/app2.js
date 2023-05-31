de3c8e040db1152e5505eef70fb9101f3b7fc500
savek1 <cod2roxy@gmail.com>
2023-05-31 20:52:32 +0300

    Add files via upload

---
+++ test/app2.js
@@ -0,0 +1,24 @@
+let tg = Telegram.WebApp;
+
+        
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+document.addEventListener("change", function(){
+    tg.MainButton.setText("Save");
+    tg.MainButton.show();
+})
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    let data = "";
+    let checked = document.querySelectorAll("input");
+    for (i=0;i<checked.length;i++){
+        if (checked[i].checked){
+            console.log(checked[i].id)
+            data = data +","+ checked[i].id
+        }
+    }
+    let radius = document.querySelector("select").value
+    tg.sendData(data+'_'+ radius);
+})
