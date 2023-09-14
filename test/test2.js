8bf88077a566346f4e3faa1c80c0d92978e7bbc9
savek1 <cod2roxy@gmail.com>
2023-09-15 00:40:08 +0300

    Update test2.js

--- test/test2.js
+++ test/test2.js
@@ -1,1 +1,25 @@
+let tg = Telegram.WebApp;
 
+tg.ready();
+tg.expand();
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+document.addEventListener("change", function(){
+    tg.MainButton.setText("Save");
+    tg.MainButton.show();
+})
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    let data = "";
+    let checked = document.querySelectorAll("input");
+    for (i=0;i<checked.length;i++){
+        if (checked[i].checked){
+            console.log(checked[i].id)
+            data = data +","+ checked[i].id
+        }
+    }
+    let radius = document.querySelector("#search-radius").value
+    let share_amount = document.querySelector("#share-amount").value
+    tg.sendData(data+'_'+ radius+ '_' + share_amount);
+})
