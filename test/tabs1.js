ad9041df97bda9f2a4665e0981b478ff74bd0b61
savek1 <cod2roxy@gmail.com>
2023-11-05 22:43:14 +0300

    Add files via upload

---
+++ test/tabs1.js
@@ -0,0 +1,40 @@
+let tg = Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+try {
+    tg.setHeaderColor("#2cab37");
+} catch(err) {}
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+document.addEventListener("change", function(){
+    tg.MainButton.setText("Save");
+    tg.MainButton.show();
+})
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+    let data = "";
+    let checked = document.querySelectorAll("input");
+    for (i=0;i<checked.length;i++){
+        if (checked[i].checked){
+            console.log(checked[i].id)
+            data = data +","+ checked[i].id
+        }
+    }
+    let radius = document.querySelector("#search-radius").value
+    let share_amount = document.querySelector("#share-amount").value
+    let x = document.querySelector("#x").value
+    let y = document.querySelector("#y").value
+    if (data.startsWith(',')){
+            data = data.substring(1)
+        }
+    let ret = {
+        "b": data,
+        "r": radius,
+        "s": share_amount,
+        "x": x,
+        "y": y
+    }
+    tg.sendData(JSON.stringify(ret));
+})
